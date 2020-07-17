73344f276601c1f667bd2b83d071aaa2b7558c9b
Lauren Anderson <laurenaanderson95@gmail.com>
2020-07-17 00:44:59 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,9 +13,9 @@ end
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-    binding.pry
+    
     puts "You are too old for this."
     
   end
-   
+   binding.pry
 end
